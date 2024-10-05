a923dc52667a23a3964d63bf087f4a41fee5bbd3
baraneldemir <brneldemirrr@gmail.com>
2024-10-05 13:29:34 +0300

    price change

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -34,20 +34,20 @@ const Person = mongoose.model("Person", personSchema)
 const router = Router()
 
 const storeItems = new Map([
-    [1, {priceInCents: 999, name: "Plant Hoodie"}],
-    [2, {priceInCents: 999, name: "Green Hoodie"}],
-    [3, {priceInCents: 999, name: "White Hoodie"}],
-    [4, {priceInCents: 999, name: "Bronwish Hoodie"}],
-    [5, {priceInCents: 999, name: "White Hoodie"}],
-    [6, {priceInCents: 999, name: "Red Hoodie"}],
-    [7, {priceInCents: 999, name: "White Red Hoodie"}],
-    [8, {priceInCents: 999, name: "White Blue Hoodie"}],
-    [9, {priceInCents: 999, name: "White Brown Hoodie"}],
-    [10, {priceInCents: 999, name: "White Black Hoodie"}],
-    [11, {priceInCents: 999, name: "White Gray Hoodie"}],
-    [12, {priceInCents: 999, name: "White Plant Hoodie"}],
-    [13, {priceInCents: 999, name: "White Brownish Hoodie"}],
-    [14, {priceInCents: 999, name: "White Brownishs Hoodie"}]          
+    [1, {priceInCents: 3999, name: "Plant Hoodie"}],
+    [2, {priceInCents: 3999, name: "Green Hoodie"}],
+    [3, {priceInCents: 3999, name: "White Hoodie"}],
+    [4, {priceInCents: 3999, name: "Bronwish Hoodie"}],
+    [5, {priceInCents: 3999, name: "White Hoodie"}],
+    [6, {priceInCents: 3999, name: "Red Hoodie"}],
+    [7, {priceInCents: 3999, name: "White Red Hoodie"}],
+    [8, {priceInCents: 3999, name: "White Blue Hoodie"}],
+    [9, {priceInCents: 3999, name: "White Brown Hoodie"}],
+    [10, {priceInCents: 3999, name: "White Black Hoodie"}],
+    [11, {priceInCents: 3999, name: "White Gray Hoodie"}],
+    [12, {priceInCents: 3999, name: "White Plant Hoodie"}],
+    [13, {priceInCents: 3999, name: "White Brownish Hoodie"}],
+    [14, {priceInCents: 3999, name: "White Brownishs Hoodie"}]          
     ])
 
     api.listen(port, () => {
